8186b70506d98256bb5ef9e2f4e7c4e5c16a4243
Simon Biickert <simon@biickert.ca>
2025-05-02 16:37:42 -0600

    Zone Detail VC entry is working

--- GISSimulator/UI/ViewControllers/ZoneDetailViewController.swift
+++ GISSimulator/UI/ViewControllers/ZoneDetailViewController.swift
@@ -13,18 +13,34 @@ class ZoneDetailViewController: UIViewController,
 								UITextFieldDelegate {
 	
 	@IBOutlet weak var nameTextField: UITextField!
+	@IBAction func nameChanged(_ sender: Any) {
+		zone.name = nameTextField.text ?? zone.name
+	}
 	@IBOutlet weak var descriptionTextField: UITextField!
+	@IBAction func descriptionChanged(_ sender: UITextField) {
+		zone.description = descriptionTextField.text ?? zone.description
+	}
 	
 	@IBOutlet weak var typeMenuButton: UIButton!
-	@IBAction func typeButtonClicked(_ sender: UIButton) {
+	@IBAction func typeMenuAction(_ sender: UIAction) {
+		if let t = ZoneType(rawValue: sender.title) {
+			zone.zoneType = t
+		}
 	}
+	
 	@IBOutlet weak var bandwidthTextField: UITextField!
 	@IBOutlet weak var bandwidthMenuButton: UIButton!
-	@IBAction func bandwidthButtonClicked(_ sender: UIButton) {
+	@IBAction func bandwidthMenuAction(_ sender: UIAction) {
+		localConnection?.bandwidthMbps = Int(sender.title) ?? 1
+		updateUI()
 	}
+	@IBAction func bandwidthChanged(_ sender: UITextField) {
+		localConnection?.bandwidthMbps = Int(sender.text ?? "0") ?? 1
+	}
+	
 	@IBOutlet weak var latencyTextField: UITextField!
-	@IBOutlet weak var latencyStepper: UIStepper!
-	@IBAction func latencyStepperChanged(_ sender: UIStepper) {
+	@IBAction func latencyChanged(_ sender: UITextField) {
+		localConnection?.latencyMs = Int(sender.text ?? "0") ?? 0
 	}
 	
 	@IBOutlet weak var tableview: UITableView!
@@ -42,18 +58,46 @@ class ZoneDetailViewController: UIViewController,
 	}
 	
 	private func updateUI() {
-		guard let design = design, nameTextField != nil else { return }
+		guard design != nil, nameTextField != nil else { return }
 		nameTextField.text = zone.name
 		descriptionTextField.text = zone.description
-		let local = zone.localConnection(in: design.network)
+		let local = localConnection
 		bandwidthTextField.text = String(local?.bandwidthMbps ?? 0)
 		latencyTextField.text = String(local?.latencyMs ?? 0)
+		if let selectedItem = typeMenuButton.menu!.children.first(where: { $0.title == zone.zoneType.rawValue }) as? UIAction {
+			selectedItem.state = .on
+			print(selectedItem.title)
+		}
+		if let selectedItem = bandwidthMenuButton.menu!.children.first(where: { Int($0.title) == local?.bandwidthMbps ?? 0 }) as? UIAction {
+			//selectedItem.state = .on
+			//print(selectedItem.title)
+		}
 		tableview.reloadData()
 	}
 	
+	private var _localConnection: Connection? = nil
+	private var localConnection: Connection? {
+		get {
+			guard let design = design else { return nil }
+			if _localConnection == nil {
+				_localConnection = zone.localConnection(in: design.network)
+			}
+			return _localConnection
+		}
+		set {
+			_localConnection = newValue
+		}
+	}
+	
+	private var _connections: [Connection] = []
 	private var connections: [Connection] {
 		guard let design = design else { return [] }
-		return zone.connections(in: design.network)
+		if _connections.count == 0 {
+			_connections = zone.connections(in: design.network)
+				.sorted { $0.name < $1.name }
+				.filter { $0.isLocal == false }
+		}
+		return _connections
 	}
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -73,11 +117,30 @@ class ZoneDetailViewController: UIViewController,
 		return cell
 	}
 	
+	func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+		if textField == bandwidthTextField || textField == latencyTextField {
+			let allowedCharacters = CharacterSet.decimalDigits
+			let characterSet = CharacterSet(charactersIn: string)
+			return allowedCharacters.isSuperset(of: characterSet)
+		}
+		return true
+	}
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Set up the Zone Type popup menu.
+		let options: [UIAction] = ZoneType.allCases.map({ UIAction(title: $0.rawValue, handler: {[self] (action: UIAction) in
+			self.typeMenuAction(action)
+		}) })
+		typeMenuButton.menu = UIMenu(children: options)
+		
+		// Set up the bandwidth options menu.
+		let bandwidthOptions: [UIAction] = [10, 100, 500, 1000, 2500, 10000].map({ UIAction(title: String($0), handler: {[self] (action: UIAction) in
+			self.bandwidthMenuAction(action)
+		}) })
+		bandwidthMenuButton.menu = UIMenu(children: bandwidthOptions)
+		
 		updateUI()
     }
     
